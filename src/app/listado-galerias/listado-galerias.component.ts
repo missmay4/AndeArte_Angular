cbe79ba3c4f079ac63b3b2db52dfd1eaf27857d2
Mayte <may.eater@gmail.com>
2018-02-13 22:42:26 +0100

    prueba clase galeria nombre e img

--- src/app/listado-galerias/listado-galerias.component.ts
+++ src/app/listado-galerias/listado-galerias.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Galeria } from '../galeria'; // importar la clase que está en galeria.ts
 
 @Component({
   selector: 'app-listado-galerias',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoGaleriasComponent implements OnInit {
 
+  const galerias_cercanas: Galeria[] = [
+    { name: 'Magda Bellotti', photo: 'assets/img/galeriamagdamadrid.jpg' }, // Datos galerias index, por orden
+    { name: 'Bacelos', photo: 'assets/img/galeriabacelosmadrid.jpg'},
+    { name: 'Art Sage', photo: 'assets/img/fotogaleria.jpg'},
+    { name: 'Arde Arte', photo: 'assets/img/ardearte.jpg'}
+  ];
   constructor() { }
 
   ngOnInit() {
