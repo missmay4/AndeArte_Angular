f3321f627904dd0f57ee8205eb8a7a297cdbdaac
Mayte <may.eater@gmail.com>
2018-02-15 17:06:35 +0100

    Links SPA

--- src/app/vista-detalle/vista-detalle.component.ts
+++ src/app/vista-detalle/vista-detalle.component.ts
@@ -10,16 +10,6 @@ import { BuscadorComponent } from '../buscador/buscador.component';
 })
 export class VistaDetalleComponent implements OnInit {
 
-/*      name: 'Magda Belloti', // nombre galeria index
-       //photo: string, // url foto galeria index
-       description: 'Fundada en 1980, Galería Magda Bellotti nace con el objetivo de dar a conocer el arte en el corazón de la capital española. Especializada en pinturas de los siglos XIX y XX.',
-       //imagenes: string,
-       direccion: 'Calle Fúcar nº22 28014 Madrid',
-       horario: 'M-V 10:00-14:00 | 17:00-20:00 | S 10:00-14:00',
-       telefono: 913693717,
-       web: 'www.magdabellotti.com',
-       email: 'galeriamagdabellotti@gmail.com' */
-
   constructor() { }
 
   ngOnInit() {
