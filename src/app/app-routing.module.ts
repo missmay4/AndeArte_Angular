e2d1693a14c53625d8ad43bcad22050ef93ebd5f
Unknown <prinpio@gmail.com>
2018-02-15 13:08:55 +0100

    Routing I y cambio vistas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListadoGaleriasComponent } from './listado-galerias/listado-galerias.component';
 import { FormularioGaleriasComponent } from './formulario-galerias/formulario-galerias.component';
 import { VistaDetalleComponent } from './vista-detalle/vista-detalle.component';
+import { PerfiluserComponent } from './perfiluser/perfiluser.component';
+import { CreargaleriaComponent } from './creargaleria/creargaleria.component';
+import { EditargaleriaComponent } from './editargaleria/editargaleria.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/listado-galerias', pathMatch: 'full'},
   { path: 'listado-galerias', component: ListadoGaleriasComponent },
   { path: 'formulario-galerias', component: FormularioGaleriasComponent },
-  { path: 'vista-detalle', component: VistaDetalleComponent}
+  { path: 'vista-detalle', component: VistaDetalleComponent},
+  { path: 'perfiluser', component: PerfiluserComponent },
+  { path: 'creargaleria', component: CreargaleriaComponent },
+  { path: 'editargaleria', component: EditargaleriaComponent },
+
 ];
 
 @NgModule({
